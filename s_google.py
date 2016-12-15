3bd553dd00421b1c1f859ad281d3e09880495a97
lordbendtner <nosseir.michel@gmail.com>
2016-12-15 18:57:56 +0100

    update

--- s_google.py
+++ s_google.py
@@ -1,27 +1,20 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+from wtforms import Form, BooleanField, TextField, validators
 app = Flask(__name__)
+
+class RegistrationForm(Form):
+    username     = TextField('Username', [validators.Length(min=4, max=25)])
+    email        = TextField('Email Address', [validators.Length(min=6, max=35)])
+    accept_rules = BooleanField('I accept the site rules', [validators.Required()])
 
 @app.route('/', methods=['POST'])
 def my_form_post():
 
-    processed_text = request.form['text'].upper()
-    return render_template('index.html')
+    processed_text = request.form['text']
+    return render_template('index.html', resultat=processed_text)
 
 @app.route('/')
 def hello():
     return render_template('index.html')
-
-
-# -*-coding:Latin-1 -*
-
-
-import os # On importe le module os qui dispose de variables 
-          # et de fonctions utiles pour dialoguer avec votre 
-          # système d'exploitation
-
-# Programme testant si une année, saisie par l'utilisateur, est bissextile ou non
-
-# On met le programme en pause pour éviter qu'il ne se referme (Windows)
-os.system("pause")
