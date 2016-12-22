c5fa8fc7ab3940bc9dd09500e88b34b3f5d835a8
lordbendtner <nosseir.michel@gmail.com>
2016-12-22 22:54:37 +0100

    up

--- s_google.py
+++ s_google.py
@@ -1,22 +1,62 @@
 from flask import Flask
-from flask import request
+from flask import request,url_for
 import requests 
 from flask import render_template
-from wtforms import Form, BooleanField, TextField, validators
+from html.parser import HTMLParser
+from wtforms import Form, BooleanField, TextField, validators, TextAreaField
 app = Flask(__name__)
 
 class RegistrationForm(Form):
-    username     = TextField('Username', [validators.Length(min=4, max=25)])
-    email        = TextField('Email Address', [validators.Length(min=6, max=35)])
-    accept_rules = BooleanField('I accept the site rules', [validators.Required()])
+    url    = TextField('url')
+    motscles = TextAreaField('motscles',render_kw={"rows": 11, "cols": 20})
+    position = TextField('position')
+    page_max = TextField('page_max')
+
+class MyHTMLParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.inLink = False
+        self.data = []
+        self.position = 0
+        self.countLanguages = 0
+        self.lasttag = None
+        self.lastname = None
+        self.lastvalue = None
+        self.form = RegistrationForm(request.form)
+
+    def handle_starttag(self, tag, attrs):
+        self.inLink = False
+        if tag == 'cite':
+            for name, value in attrs:
+                if name == 'class' and value == '_Rm':
+                    self.inLink = True
+                    self.lasttag = tag
+
+    def handle_endtag(self, tag):
+        if tag == "cite":
+            self.inlink = False
+
+    def handle_data(self, data):
+        if self.lasttag == 'cite' and data.find(self.form.url.data) != -1:
+            self.data.append(data)
+            self.position = self.getpos()[0] - 1
+            print (data)
+            print(self.getpos()[0] - 1)
+
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-
-    processed_text = "https://www.google.fr/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q="+request.form['text']
+    form = RegistrationForm(request.form)
+    processed_text = "https://www.google.fr/search?hl=fr&q="+form.motscles.data[0]+"&start=0"
+    parse = MyHTMLParser()
     r = requests.get(processed_text)
-    return render_template('index.html', resultat=processed_text)
+    res = parse.feed(r.text)
+
+
+    return render_template('index.html', form=form, resultat=parse.data, position=parse.position)
 
 @app.route('/')
+
 def hello():
-    return render_template('index.html')
+    form = RegistrationForm(request.form)
+    return render_template('index.html',form=form)
