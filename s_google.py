bca3b3bc23c9592fa754339af2d69a811cc5fa3a
lordbendtner <nosseir.michel@gmail.com>
2016-12-23 03:42:53 +0100

    up

--- s_google.py
+++ s_google.py
@@ -3,12 +3,12 @@ from flask import request,url_for
 import requests 
 from flask import render_template
 from html.parser import HTMLParser
-from wtforms import Form, BooleanField, TextField, validators, TextAreaField
+from wtforms import Form, BooleanField, TextField, validators, TextAreaField , FieldList, IntegerField
 app = Flask(__name__)
 
 class RegistrationForm(Form):
     url    = TextField('url')
-    motscles = TextAreaField('motscles',render_kw={"rows": 11, "cols": 20})
+    motscles = TextAreaField('motscles',render_kw={"rows": 11, "cols": 30})
     position = TextField('position')
     page_max = TextField('page_max')
 
@@ -16,7 +16,7 @@ class MyHTMLParser(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
         self.inLink = False
-        self.data = []
+        self.data = 'n/a'
         self.position = 0
         self.countLanguages = 0
         self.lasttag = None
@@ -38,25 +38,44 @@ class MyHTMLParser(HTMLParser):
 
     def handle_data(self, data):
         if self.lasttag == 'cite' and data.find(self.form.url.data) != -1:
-            self.data.append(data)
-            self.position = self.getpos()[0] - 1
-            print (data)
-            print(self.getpos()[0] - 1)
+            self.data = data
+            self.position = self.getpos()[0]
 
 
 @app.route('/', methods=['POST'])
 def my_form_post():
     form = RegistrationForm(request.form)
-    processed_text = "https://www.google.fr/search?hl=fr&q="+form.motscles.data[0]+"&start=0"
-    parse = MyHTMLParser()
-    r = requests.get(processed_text)
-    res = parse.feed(r.text)
+    page  = form.page_max.data
+    i = 0;
+    donne_final = '<table class="tableau" style=" border : solid 1px black;text-align: center;"><tr><td><b>Mot-clé</b></td><td><b>URL</b></td><td><b>Position</b></td></tr>'
+    form.motscles.data.split("\n")
+    for l in form.motscles.data.split("\n"):
+        while i < (int(page)*10):
+            processed_text = "https://www.google.fr/search?hl=fr&q="+l+"&start="+str(i)
+            parse = MyHTMLParser()
+            r = requests.get(processed_text)
+            res = parse.feed(r.text)
+            if parse.data  != 'n/a' or (i+10)  >= (int(page)*10):
+                donne_final = donne_final+"<tr><td>"
+                donne_final = donne_final+l
+                donne_final = donne_final+"</td><td>"
+                donne_final = donne_final+parse.data
+                donne_final = donne_final+"</td><td>"
+                if parse.data  != 'n/a':
+                    donne_final = donne_final+str(parse.position+i)
+                else:
+                    donne_final = donne_final+'n/a'
+                donne_final = donne_final+"</td></tr>"
+                break
+            i = i+10
+    donne_final  = donne_final+"</table>"
 
-
-    return render_template('index.html', form=form, resultat=parse.data, position=parse.position)
+    return render_template('index.html', form=form, resultat=donne_final, position=parse.position)
 
 @app.route('/')
 
 def hello():
     form = RegistrationForm(request.form)
+    form.page_max.default = "1"
+    form.process()
     return render_template('index.html',form=form)
